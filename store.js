6c5930339764dcd26a00d862bfb614a6e3c7ccb6
Prachi I <prachi.indurkar007@gmail.com>
2023-04-04 17:47:56 +0530

    use the ₹ symbol for rupees

--- store.js
+++ store.js
@@ -81,12 +81,12 @@ function updateCartTotal() {
         var cartRow = cartRows[i];
         var priceElement = cartRow.getElementsByClassName("cart-price")[0]
         var quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0]
-        var price = parseFloat(priceElement.innerText.replace("$", ""))
+        var price = parseFloat(priceElement.innerText.replace("₹", ""))
         var quantity = quantityElement.value
         total = total + (price * quantity)
     }
     total = Math.round(total * 100) / 100
-    document.getElementsByClassName("cart-total-price")[0].innerText = "$" + total;
+    document.getElementsByClassName("cart-total-price")[0].innerText = "₹" + total;
 }
 
 document.getElementsByClassName("btn-purchase")[0].addEventListener("click", purchaseClicked)
